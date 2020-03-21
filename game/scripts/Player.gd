0aae43d10fb11b5a9c02f7fedb1c6b2fd1665bbf
Eric Rodríguez Balsells <erick29496@gmail.com>
2020-03-21 21:13:41 +0100

    Add camera and player movement

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
  
-const MOVE_SPEED = 500
 const JUMP_FORCE = 500
 const GRAVITY = 50
 const MAX_FALL_SPEED = 1000
@@ -10,16 +9,14 @@ onready var sprite = $AnimatedSprite
 var y_velo = 0
 var facing_right = false
 var fuel = 100
+
+var time = 0
+var futureTime = 0
  
 func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("ui_right"):
-		move_dir += 1
-		$AnimatedSprite.play()
-	if Input.is_action_pressed("ui_left"):
-		move_dir -= 1
-		$AnimatedSprite.play()
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+	$AnimatedSprite.play()
+	var walkSpeed = get_parent().get_node("Camera2D").getSpeed()
+	move_and_slide(Vector2(walkSpeed, y_velo), Vector2(0, -1))
 
 	var grounded = is_on_floor()
 	if grounded:
@@ -34,12 +31,3 @@ func _physics_process(delta):
 		y_velo = 5
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
-   
-	if !facing_right and move_dir < 0:
-		flip()
-	if facing_right and move_dir > 0:
-		flip()
- 
-func flip():
-	facing_right = !facing_right
-	sprite.flip_h = !sprite.flip_h
