cbfc4e8215d038c4d8cb95036a0d8480cb935c42
unknown <danicv94@gmail.com>
2020-03-21 21:00:55 +0100

    add camera

---
+++ game/scripts/camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var WALK_SPEED = 300
+var time = 0
+var futureTime = 0
+
+func _ready():
+	set_process(true)
+	
+func getSpeed():
+	return self.WALK_SPEED
+
+func _process(delta):
+	time += delta
+	if futureTime == 0:
+		futureTime = time + 5
+	position.x += WALK_SPEED * delta
+	if time >= futureTime:
+		WALK_SPEED += 50
+		futureTime = 0
