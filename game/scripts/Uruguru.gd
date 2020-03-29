e6453bc45a0e63beb8dac86c794d3df0d1054cf8
Eric Rodríguez Balsells <erick29496@gmail.com>
2020-03-29 13:53:14 +0200

    add uruguru scene

---
+++ game/scripts/Uruguru.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Uruguru_body_entered(body):
+	get_parent().get_parent().get_node('Player').die()
