a38a64a6e4c205024f51364cf37bab105d2c0c87
Eric Rodríguez Balsells <erick29496@gmail.com>
2020-04-23 19:44:22 +0200

    Fix upgrade fuel in shop

--- game/scripts/Shop.gd
+++ game/scripts/Shop.gd
@@ -13,5 +13,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	get_parent().get_node("Score").text = str(Global.get_shopingPoints()) + " puntos"
+	get_parent().get_node("Fuel").text = "Fuel: " + str(Global.get_fuel())
